b345dddbbdfab7aeb292fe9ce7efa59dd709182f
Shubham Sahai <shubham@Shubhams-MacBook-Air.local>
2016-05-24 21:59:51 +0530

    updated the urls to load the index of polls

--- poll_app/polls/views.py
+++ poll_app/polls/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+
+def index(request):
+    print request
+    title = "<center><h1>Hello, world. Welcome to index page of Polls !</h1></center>"
+    return HttpResponse(title)
